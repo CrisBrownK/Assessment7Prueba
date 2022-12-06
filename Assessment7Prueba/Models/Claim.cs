341607c7cf8c2b0bfa9a9e84d76b1a7b31cfe145
Cris <criguzmon@gmail.com>
2022-12-06 21:39:20 +0100

    Agregar archivos de proyecto.

---
+++ Assessment7Prueba/Models/Claim.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+
+namespace Assessment7Prueba.Models;
+
+public partial class Claim
+{
+    public int Id { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public DateTime Date { get; set; }
+
+    public int VehicleId { get; set; }
+
+    [ValidateNever]
+    public virtual Vehicle Vehicle { get; set; } = null!;
+}
